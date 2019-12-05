c6dd97af50c6ebd601c530ba59bc5411eec32ec5
Adrien Foulon <6115458+Tofandel@users.noreply.github.com>
2019-12-05 17:55:05 +0100

    Fix reference issue and add delays for all open/close action

--- packages/popover/src/main.vue
+++ packages/popover/src/main.vue
@@ -80,62 +80,28 @@ export default {
         return;
       }
       val ? this.$emit('show') : this.$emit('hide');
+    },
+    trigger() {
+      this.removeEvents();
+      this.createEvents();
     }
   },
 
   mounted() {
-    let reference = this.referenceElm = this.reference || this.$refs.reference;
-    const popper = this.popper || this.$refs.popper;
+    this.createEvents();
+  },
 
-    if (!reference && this.$slots.reference && this.$slots.reference[0]) {
-      reference = this.referenceElm = this.$slots.reference[0].elm;
-    }
-    // 可访问性
-    if (reference) {
-      addClass(reference, 'el-popover__reference');
-      reference.setAttribute('aria-describedby', this.tooltipId);
-      reference.setAttribute('tabindex', this.tabindex); // tab序列
-      popper.setAttribute('tabindex', 0);
+  beforeUpdate() {
+    this.removeEvents();
+  },
 
-      if (this.trigger !== 'click') {
-        on(reference, 'focusin', () => {
-          this.handleFocus();
-          const instance = reference.__vue__;
-          if (instance && typeof instance.focus === 'function') {
-            instance.focus();
-          }
-        });
-        on(popper, 'focusin', this.handleFocus);
-        on(reference, 'focusout', this.handleBlur);
-        on(popper, 'focusout', this.handleBlur);
-      }
-      on(reference, 'keydown', this.handleKeydown);
-      on(reference, 'click', this.handleClick);
-    }
-    if (this.trigger === 'click') {
-      on(reference, 'click', this.doToggle);
-      on(document, 'click', this.handleDocumentClick);
-    } else if (this.trigger === 'hover') {
-      on(reference, 'mouseenter', this.handleMouseEnter);
-      on(popper, 'mouseenter', this.handleMouseEnter);
-      on(reference, 'mouseleave', this.handleMouseLeave);
-      on(popper, 'mouseleave', this.handleMouseLeave);
-    } else if (this.trigger === 'focus') {
-      if (this.tabindex < 0) {
-        console.warn('[Element Warn][Popover]a negative taindex means that the element cannot be focused by tab key');
-      }
-      if (reference.querySelector('input, textarea')) {
-        on(reference, 'focusin', this.doShow);
-        on(reference, 'focusout', this.doClose);
-      } else {
-        on(reference, 'mousedown', this.doShow);
-        on(reference, 'mouseup', this.doClose);
-      }
-    }
+  updated() {
+    this.createEvents();
   },
 
   beforeDestroy() {
     this.cleanup();
+    this.removeEvents();
   },
 
   deactivated() {
@@ -143,27 +109,75 @@ export default {
   },
 
   methods: {
+    createEvents() {
+      const popper = this.popper || this.$refs.popper;
+      const reference = this.getReference();
+
+      // 可访问性
+      if (reference) {
+        addClass(reference, 'el-popover__reference');
+        reference.setAttribute('aria-describedby', this.tooltipId);
+        reference.setAttribute('tabindex', this.tabindex); // tab序列
+        popper.setAttribute('tabindex', 0);
+
+        if (this.trigger !== 'click') {
+          on(reference, 'focusin', () => {
+            this.handleFocus();
+            const instance = reference.__vue__;
+            if (instance && typeof instance.focus === 'function') {
+              instance.focus();
+            }
+          });
+          on(popper, 'focusin', this.handleFocus);
+          on(reference, 'focusout', this.handleBlur);
+          on(popper, 'focusout', this.handleBlur);
+        }
+        on(reference, 'keydown', this.handleKeydown);
+        on(reference, 'click', this.handleClick);
+      }
+      if (this.trigger === 'click') {
+        on(reference, 'click', this.doToggle);
+        on(document, 'click', this.handleDocumentClick);
+      } else if (this.trigger === 'hover') {
+        on(reference, 'mouseenter', this.handleMouseEnter);
+        on(popper, 'mouseenter', this.handleMouseEnter);
+        on(reference, 'mouseleave', this.handleMouseLeave);
+        on(popper, 'mouseleave', this.handleMouseLeave);
+      } else if (this.trigger === 'focus') {
+        if (this.tabindex < 0) {
+          console.warn('[Element Warn][Popover]a negative tabindex means that the element cannot be focused by tab key');
+        }
+        if (reference.querySelector('input, textarea')) {
+          on(reference, 'focusin', this.doShow);
+          on(reference, 'focusout', this.doClose);
+        } else {
+          on(reference, 'mousedown', this.doShow);
+          on(reference, 'mouseup', this.doClose);
+        }
+      }
+    },
+    removeEvents() {
+      const reference = this.getReference();
+
+      off(reference, 'click', this.doToggle);
+      off(reference, 'mouseup', this.doClose);
+      off(reference, 'mousedown', this.doShow);
+      off(reference, 'focusin', this.doShow);
+      off(reference, 'focusout', this.doClose);
+      off(reference, 'mousedown', this.doShow);
+      off(reference, 'mouseup', this.doClose);
+      off(reference, 'mouseleave', this.handleMouseLeave);
+      off(reference, 'mouseenter', this.handleMouseEnter);
+      off(document, 'click', this.handleDocumentClick);
+    },
     doToggle() {
-      this.showPopper = !this.showPopper;
+      if (this.showPopper) {
+        this.doClose();
+      } else {
+        this.doShow();
+      }
     },
     doShow() {
-      this.showPopper = true;
-    },
-    doClose() {
-      this.showPopper = false;
-    },
-    handleFocus() {
-      addClass(this.referenceElm, 'focusing');
-      if (this.trigger === 'click' || this.trigger === 'focus') this.showPopper = true;
-    },
-    handleClick() {
-      removeClass(this.referenceElm, 'focusing');
-    },
-    handleBlur() {
-      removeClass(this.referenceElm, 'focusing');
-      if (this.trigger === 'click' || this.trigger === 'focus') this.showPopper = false;
-    },
-    handleMouseEnter() {
       clearTimeout(this._timer);
       if (this.openDelay) {
         this._timer = setTimeout(() => {
@@ -173,12 +187,7 @@ export default {
         this.showPopper = true;
       }
     },
-    handleKeydown(ev) {
-      if (ev.keyCode === 27 && this.trigger !== 'manual') { // esc
-        this.doClose();
-      }
-    },
-    handleMouseLeave() {
+    doClose() {
       clearTimeout(this._timer);
       if (this.closeDelay) {
         this._timer = setTimeout(() => {
@@ -188,13 +197,32 @@ export default {
         this.showPopper = false;
       }
     },
+    handleFocus() {
+      addClass(this.getReference(), 'focusing');
+      if (this.trigger === 'click' || this.trigger === 'focus') this.showPopper = true;
+    },
+    handleClick() {
+      removeClass(this.getReference(), 'focusing');
+    },
+    handleBlur() {
+      removeClass(this.getReference(), 'focusing');
+      if (this.trigger === 'click' || this.trigger === 'focus') this.showPopper = false;
+    },
+    handleMouseEnter() {
+      this.doShow();
+    },
+    handleKeydown(ev) {
+      if (ev.keyCode === 27 && this.trigger !== 'manual') { // esc
+        this.doClose();
+      }
+    },
+    handleMouseLeave() {
+      this.doClose();
+    },
     handleDocumentClick(e) {
-      let reference = this.reference || this.$refs.reference;
+      const reference = this.getReference();
       const popper = this.popper || this.$refs.popper;
 
-      if (!reference && this.$slots.reference && this.$slots.reference[0]) {
-        reference = this.referenceElm = this.$slots.reference[0].elm;
-      }
       if (!this.$el ||
         !reference ||
         this.$el.contains(e.target) ||
@@ -216,20 +244,5 @@ export default {
       }
     }
   },
-
-  destroyed() {
-    const reference = this.reference;
-
-    off(reference, 'click', this.doToggle);
-    off(reference, 'mouseup', this.doClose);
-    off(reference, 'mousedown', this.doShow);
-    off(reference, 'focusin', this.doShow);
-    off(reference, 'focusout', this.doClose);
-    off(reference, 'mousedown', this.doShow);
-    off(reference, 'mouseup', this.doClose);
-    off(reference, 'mouseleave', this.handleMouseLeave);
-    off(reference, 'mouseenter', this.handleMouseEnter);
-    off(document, 'click', this.handleDocumentClick);
-  }
 };
 </script>
